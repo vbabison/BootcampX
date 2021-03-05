2bd39d0c0083de25a29fb50ebbd4b0546aa080eb
Vbabison <officialbookingssf@gmail.comm>
2021-03-05 05:41:41 -0500

    Node Postgres

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,8 +11,14 @@ const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5}`;
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
 pool.query(queryString, values)
 .then(res => {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,14 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString = ` SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher
+LIMIT $2;
 `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+const values = [`%${cohortName}%`, limit];
 
 pool.query(queryString, values)
 .then(res => {
